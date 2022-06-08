ead705f7dec43ab568d850edbcd36dd80be8fe7a
snsakib <hello@snsakib.com>
2022-06-09 04:12:40 +0600

    setup product list component

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule],
-  exports: [MatIconModule]
+  imports: [CommonModule, MatIconModule, MatGridListModule, MatCardModule],
+  exports: [MatIconModule, MatGridListModule, MatCardModule],
 })
 export class AngularMaterialModule {}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

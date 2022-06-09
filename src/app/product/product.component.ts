a92e9e9ca607b0557a84584cf1748e755897dc35
snsakib <hello@snsakib.com>
2022-06-10 02:54:32 +0600

    setup product details component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  id: number = 0;
+  product: Product = {
+    id: 0,
+    title: '',
+    author: '',
+    description: '',
+    imgUrl: '',
+    quantity: 0,
+    price: 0,
+  };
 
-  constructor() { }
+  constructor(
+    private productService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.route.params.subscribe((params) => (this.id = params['id']));
 
+    this.productService
+      .getProduct(this.id)
+      .subscribe((product: Product) => (this.product = product));
+  }
 }

7b752c0be5ac985c3e66c7ae82290e892bd77cb5
snsakib <hello@snsakib.com>
2022-06-10 01:21:51 +0600

    setup product module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  {
+    path: 'product/:id',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

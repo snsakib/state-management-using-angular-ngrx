cdf95a5fada05187773cdb77b5858094fc20c306
snsakib <hello@snsakib.com>
2022-06-10 02:54:32 +0600

    setup product details component

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule, MatGridListModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   exports: [MatIconModule, MatGridListModule, MatCardModule, MatButtonModule],
 })
 export class AngularMaterialModule {}

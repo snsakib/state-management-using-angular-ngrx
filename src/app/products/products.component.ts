5d5e120109eb34ebec005eea1769ca83ae473af1
snsakib <hello@snsakib.com>
2022-07-03 16:19:32 +0600

    dispatch actions

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Product } from '../product';
+import { Store } from '@ngrx/store';
+import { getProductsAction } from './state/products.actions';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -9,7 +11,7 @@ import { Product } from '../product';
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productsService: ProductsService) {}
+  constructor(private store: Store, private productsService: ProductsService) {}
 
   ngOnInit() {
     this.getProducts();
@@ -24,6 +26,6 @@ export class ProductsComponent implements OnInit {
   getProducts() {
     this.productsService
       .getProducts()
-      .subscribe((products: Product[]) => (this.products = products));
+      .subscribe((products: Product[]) => (this.store.dispatch(getProductsAction({ products }))));
   }
 }

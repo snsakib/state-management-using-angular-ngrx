ca0d9252c886abce38d58b9fc7cb53f7b278cf5b
snsakib <hello@snsakib.com>
2022-07-25 15:45:37 +0600

    add cart icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'book-store';
+  cartTotal = 5;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService.getShoppingCart().subscribe((cartItems) => {
+      this.cartTotal = cartItems.length;
+    });
+  }
 }

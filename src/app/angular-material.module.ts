091fc900f810d5992bc697c5401cce7e416cdb1e
snsakib <hello@snsakib.com>
2022-08-06 09:43:59 +0600

    update

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatBadgeModule } from '@angular/material/badge';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatIconModule,
-    MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatBadgeModule,
   ],
-  exports: [
-    MatIconModule,
-    MatGridListModule,
-    MatCardModule,
-    MatButtonModule,
-    MatBadgeModule,
-  ],
+  exports: [MatCardModule, MatButtonModule],
 })
 export class AngularMaterialModule {}

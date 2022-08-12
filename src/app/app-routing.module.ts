3ddfcb1a01b10497fb0eff94fec96acecc34531e
snsakib <hello@snsakib.com>
2022-08-12 08:21:40 +0600

    add product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
-  { path: '', component: ProductsComponent }
+  { path: '', component: ProductsComponent },
+  {
+    path: 'product/:id',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  }
 ];
 
 @NgModule({
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent implements OnInit {
+  id: number = 0;
+  product: Product = {
+    id: 0,
+    title: '',
+    author: '',
+    description: '',
+    imgUrl: '',
+    quantity: 0,
+    price: 0,
+    cart: 0
+  };
+
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => (this.id = params['id']));
+
+    this.getProduct(this.id);
+  }
+
+  getProduct(id: number) {
+    this.productsService
+      .getProduct(id)
+      .subscribe((product: Product) => (this.product = product));
+  }
+
+  addToCart(product: Product) {
+    let updatedProduct = {...product, cart: product.cart + 1};
+    this.productsService.addToShoppingCart(updatedProduct).subscribe();
+  }
+}

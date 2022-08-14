8d10aa17055b8c3a82fef76ed9bdd413fc5a2e34
snsakib <hello@snsakib.com>
2022-08-14 18:13:13 +0600

    update state

--- src/app/product/state/product.state.ts
+++ src/app/product/state/product.state.ts
@@ -1,10 +1,1 @@
-export const productInitialState = <any>{
-  id: 0,
-  title: '',
-  author: '',
-  description: '',
-  imgUrl: '',
-  quantity: 0,
-  price: 0,
-  cart: 0,
-};
+export const productInitialState = <any>{};

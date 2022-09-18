4f8d64882f5cb078127d3ec3a4567806048e47d6
snsakib <hello@snsakib.com>
2022-09-18 19:33:11 +0600

    strongly typing the product action

--- src/app/product/state/product.actions.ts
+++ src/app/product/state/product.actions.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from "@ngrx/store";
+import { Product } from "src/app/app.interfaces";
 
 export const getProductAction = createAction(
   '[Product] Get Product',
-  props<any>()
+  props<{ product: Product }>()
 )

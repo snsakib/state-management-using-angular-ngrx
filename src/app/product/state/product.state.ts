2d0a40ffd05cbae35a0fb24b883898b20f008ebc
snsakib <hello@snsakib.com>
2022-09-18 21:14:16 +0600

    strongly typing the state

--- src/app/product/state/product.state.ts
+++ src/app/product/state/product.state.ts
@@ -1,1 +1,12 @@
-export const productInitialState = <any>{};
+import { Product } from 'src/app/app.interfaces';
+
+export const productInitialState: Product = {
+  id: 0,
+  title: '',
+  author: '',
+  description: '',
+  imgUrl: '',
+  price: 0,
+  quantity: 0,
+  cart: 0,
+};

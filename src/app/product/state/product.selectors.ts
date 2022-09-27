1fed393594ceb6a272f2b1e784cc109faa9c5e05
snsakib <hello@snsakib.com>
2022-09-27 19:44:02 +0600

    update selector

--- src/app/product/state/product.selectors.ts
+++ src/app/product/state/product.selectors.ts
@@ -1,5 +1,4 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { Product } from "src/app/app.interfaces";
 import { getProductsSelector } from "src/app/products/state/products.selectors";
 import { ProductState } from "./product.interfaces";
 
@@ -9,25 +8,23 @@ export const getProductInfoSelector = createSelector(
   getProductsSelector,
   getProductSelector,
   (products, productId) => {
-    let selectedProduct: Product = {
-      id: 0,
-      title: "",
-      author: "",
-      description: "",
-      imgUrl: "",
-      price: 0,
-      quantity: 0,
-      cart: 0
-    }
 
-    let filteredProduct = products.find((product) => product.id === productId);
+    let selectedProduct = products.find((product) => product.id === productId);
 
-    if(filteredProduct) {
-      selectedProduct = {
-        ...filteredProduct
+    if(!selectedProduct) {
+      return {
+        id: 0,
+        title: "",
+        author: "",
+        description: "",
+        imgUrl: "",
+        price: 0,
+        quantity: 0,
+        cart: 0
       }
-    }
+    } else {
+      return selectedProduct;
+    } 
 
-    return selectedProduct;
   }
 )

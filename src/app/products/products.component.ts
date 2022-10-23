b53f37d416be68954208abfe37a9501f55cc4166
snsakib <hello@snsakib.com>
2022-10-24 02:27:49 +0600

    comment out the removed codes

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { ProductsService } from '../products.service';
 import { Store } from '@ngrx/store';
+// import { getProductsAction } from './state/products.actions';
 import { getProductsSelector } from './state/products.selectors';
 import { AppState, Product } from '../app.interfaces';
 @Component({
@@ -10,13 +12,18 @@ import { AppState, Product } from '../app.interfaces';
 export class ProductsComponent implements OnInit {
   products$ = this.store.select<Product[]>(getProductsSelector);
 
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>, 
+    // private productsService: ProductsService
+  ) {}
 
   ngOnInit() {
     this.getProducts();
   }
 
   getProducts() {
-
+    // this.productsService
+    //   .getProducts()
+    //   .subscribe((products: Product[]) => (this.store.dispatch(getProductsAction({ products }))));
   }
 }

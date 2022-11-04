8313a646d32ff8a533b5ac48532a5a25297aec73
snsakib <hello@snsakib.com>
2022-11-04 07:05:21 +0600

    testing productsReducer

---
+++ src/app/products/state/products.reducers.spec.ts
@@ -0,0 +1,28 @@
+import { Product } from 'src/app/app.interfaces';
+import { getProductsAction } from './products.actions';
+import { productsReducer } from './products.reducers';
+import { productsInitialState } from './products.state';
+
+describe('ProductsReducers', () => {
+  describe('getProductsAction', () => {
+    it('should update the state in an immutable way', () => {
+      const newState: Product[] = [
+        {
+          id: 1,
+          title: 'The Angular Masterclass',
+          author: 'Educative',
+          description: 'Learn all about Angular',
+          imgUrl: '../assets/img/the-angular-masterclass.jpg',
+          quantity: 124,
+          price: 24.38,
+          cart: 0,
+        }
+      ];
+      const action = getProductsAction({ products: newState });
+      const state = productsReducer(productsInitialState, action);
+
+      expect(state).toEqual(newState);
+      expect(state).not.toBe(newState);
+    });
+  });
+});

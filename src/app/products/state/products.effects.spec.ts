fbe06d7c26203b39fa0933360ad96090a246d490
snsakib <hello@snsakib.com>
2022-11-09 09:50:43 +0600

    handle success case

--- src/app/products/state/products.effects.spec.ts
+++ src/app/products/state/products.effects.spec.ts
@@ -2,10 +2,12 @@ import { TestBed } from "@angular/core/testing"
 import { Actions } from "@ngrx/effects";
 import { provideMockActions } from "@ngrx/effects/testing";
 import { TestScheduler } from "rxjs/testing";
+import { Product } from "src/app/app.interfaces";
 import { ProductsService } from "src/app/products.service";
+import { getProductsAction, loadProductsAction } from "./products.actions";
 import { ProductsEffects } from "./products.effects";
 
-describe('ProductsEffects', () => {
+describe('ProductsEffects: loadProducts$', () => {
   let effects: ProductsEffects;
   let actions$: Actions;
   let productsServiceSpy = jasmine.createSpyObj('ProductsService', ['getProducts']);
@@ -25,6 +27,32 @@ describe('ProductsEffects', () => {
     testScheduler = new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected);
     });
+  });
+
+  it('should handle loadProductsAction and return getProductsAction', () => {
+    const products: Product[] = [
+      {
+        id: 1,
+        title: 'The Angular Masterclass',
+        author: 'Educative',
+        description: 'Learn all about Angular',
+        imgUrl: '../assets/img/the-angular-masterclass.jpg',
+        quantity: 124,
+        price: 24.38,
+        cart: 0,
+      }
+    ];
+    let action = loadProductsAction();
+    let outcome = getProductsAction({ products });
+
+    testScheduler.run(({ hot, cold, expectObservable }) => {
+      actions$ = hot('-a', { a: action });
+      let response = cold('-b|', { b: products });
+
+      productsServiceSpy.getProducts.and.returnValue(response);
+
+      expectObservable(effects.loadProducts$).toBe('--c', { c: outcome })
+    })
   });
 
   

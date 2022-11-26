439c63c48d161fb436b839b4def2c0911e73946f
snsakib <hello@snsakib.com>
2022-11-26 16:18:39 +0600

    update cart layout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,27 @@
-  import { Component, OnInit } from '@angular/core';
-  
-  @Component({
-    selector: 'app-cart',
-    templateUrl: './cart.component.html',
-    styleUrls: ['./cart.component.scss']
-  })
-  export class CartComponent implements OnInit {
-    products$ = [];
-  
-    constructor() { }
-  
-    ngOnInit(): void {
-    }
-  
-  }
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss'],
+})
+export class CartComponent implements OnInit {
+  products = [
+    {
+      id: 0,
+      title: '',
+      author: '',
+      description: '',
+      imgUrl: '',
+      quantity: 0,
+      price: 0,
+      cart: 0,
+    },
+  ];
+  total = 0;
+  cartCounter = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

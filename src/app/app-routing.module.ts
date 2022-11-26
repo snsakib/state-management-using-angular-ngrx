b8018416079d0f26834b8805af0e11dc734b34aa
snsakib <hello@snsakib.com>
2022-11-26 14:33:08 +0600

    create cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
-
-const routes: Routes = [
-  { path: '', component: ProductsComponent },
-  {
-    path: 'product/:id',
-    loadChildren: () =>
-      import('./product/product.module').then((m) => m.ProductModule),
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+  import { NgModule } from '@angular/core';
+  import { RouterModule, Routes } from '@angular/router';
+  import { CartComponent } from './cart/cart.component';
+  import { ProductsComponent } from './products/products.component';
+  
+  const routes: Routes = [
+    { path: '', component: ProductsComponent },
+    {
+      path: 'product/:id',
+      loadChildren: () =>
+        import('./product/product.module').then((m) => m.ProductModule),
+    },
+    { path: 'cart', component: CartComponent },
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AngularMaterialModule } from './angular-material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductsComponent } from './products/products.component';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { DataService } from './data.service';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { productsReducer } from './products/state/products.reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { ProductsEffects } from './products/state/products.effects';
-@NgModule({
-  declarations: [AppComponent, ProductsComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularMaterialModule,
-    BrowserAnimationsModule,
-    InMemoryWebApiModule.forRoot(DataService),
-    HttpClientModule,
-    StoreModule.forRoot({ products: productsReducer }),
-    StoreDevtoolsModule.instrument({
-      name: "Angular NgRx", 
-      maxAge: 25, 
-      logOnly: environment.production }),
-    EffectsModule.forRoot([ProductsEffects])
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+  import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { AppRoutingModule } from './app-routing.module';
+  import { AppComponent } from './app.component';
+  import { AngularMaterialModule } from './angular-material.module';
+  import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+  import { ProductsComponent } from './products/products.component';
+  import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+  import { DataService } from './data.service';
+  import { HttpClientModule } from '@angular/common/http';
+  import { StoreModule } from '@ngrx/store';
+  import { productsReducer } from './products/state/products.reducers';
+  import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+  import { environment } from '../environments/environment';
+  import { EffectsModule } from '@ngrx/effects';
+  import { ProductsEffects } from './products/state/products.effects';
+  import { CartComponent } from './cart/cart.component';
+  @NgModule({
+    declarations: [
+      AppComponent, 
+      ProductsComponent, 
+      CartComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      AngularMaterialModule,
+      BrowserAnimationsModule,
+      InMemoryWebApiModule.forRoot(DataService),
+      HttpClientModule,
+      StoreModule.forRoot({ products: productsReducer }),
+      StoreDevtoolsModule.instrument({
+        name: 'Angular NgRx',
+        maxAge: 25,
+        logOnly: environment.production,
+      }),
+      EffectsModule.forRoot([ProductsEffects]),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+  })
+  export class AppModule {}
+  

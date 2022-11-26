eab37f3a6515002d3c5a908760e9bd10f96453d8
snsakib <hello@snsakib.com>
2022-11-26 16:51:45 +0600

    add cart icon

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
-  exports: [MatCardModule, MatButtonModule],
+  exports: [
+    MatCardModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatBadgeModule
+  ],
 })
 export class AngularMaterialModule {}

39cf84e055ab1e672929662ee9f3dd64315dc942
snsakib <hello@snsakib.com>
2022-11-28 09:47:26 +0600

    update reducers

--- src/app/cart/state/cart.reducers.ts
+++ src/app/cart/state/cart.reducers.ts
@@ -1,11 +1,17 @@
 import { createReducer, on } from '@ngrx/store';
-import { addToCartAction } from './cart.actions';
+import { addToCartAction, addToCartFailureAction, addToCartSuccessAction } from './cart.actions';
 import { CartState } from './cart.interfaces';
 import { cartInitialState } from './cart.state';
 
 export const cartReducer = createReducer<CartState>(
   cartInitialState,
   on(addToCartAction, (state): CartState => {
+    return [...state];
+  }),
+  on(addToCartSuccessAction, (state, { product }): CartState => {
+    return [...state, product];
+  }),
+  on(addToCartFailureAction, (state): CartState => {
     return [...state];
   })
 );
